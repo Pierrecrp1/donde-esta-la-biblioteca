d0beb6de01d0f2b132bff3857a200d5f15e83d2f
Alex Lovergne <lovergnealex@outlook.fr>
2024-01-29 11:06:24 +0100

    Ajout main

--- LibraryManager/LibraryManager/Program.cs
+++ LibraryManager/LibraryManager/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    private static IHost CreateHostBuilder(IConfigurationBuilder configuration)
+    {
+        return Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                // Configuration des services
+            })
+            .Build();
+    }
+}
